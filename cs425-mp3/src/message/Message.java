fb1172bfcd767374fd111766c4a421c1525a7790
huangm26 <huangm26@gmail.com>
2014-04-22 21:06:06 -0500

    forth commit

--- cs425-mp3/src/message/Message.java
+++ cs425-mp3/src/message/Message.java
@@ -1,5 +1,18 @@
 package message;
 
-public class Message {
+
+public class Message  {
+	
+	public int from;
+	public int to;
+	public int messageID;
+	public String content;
+	
+	public Message(int from, int to, int messageID, String content) {
+		this.to = to;
+		this.from = from;
+		this.messageID = messageID;
+		this.content = content;
+	}
 
 }
